cf9587b9a17371e11ed2dacd8705faba5636ed20
PHUC-PN <85268560+phuc9702@users.noreply.github.com>
2023-11-23 11:30:15 +0700

    login-signup

--- MyShoe/index.js
+++ MyShoe/index.js
@@ -1,27 +1,84 @@
 const express = require("express");
 const homeRoute = require("./routes/home");
 const mongoose = require("mongoose");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const collection = require("./models/login_signup");
+const bcrypt = require('bcrypt');
 const app = express();
-const port = 3000;
+const post = 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/shoe');
+mongoose.connect("mongodb://127.0.0.1:27017/shoe");
 const db = mongoose.connection;
 db.on("error", () => {
-  console.log("Something went wrong to connect to database")
+  console.log("Something went wrong to connect to database");
 });
 db.once("open", () => {
   console.log("DB connection has been made seccessfully");
 });
-db.one
+db.one;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use("/", homeRoute);
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//dang kí 
+app.post("/signup", async (req, res) => {
+
+  const data = {
+      name: req.body.username,
+      password: req.body.password
+  }
+
+  // Check if the username already exists in the database
+  const existingUser = await collection.findOne({ name: data.name });
+
+  if (existingUser) {
+      res.send('User already exists. Please choose a different username.');
+  } else {
+      // Hash the password using bcrypt
+      const saltRounds = 10; // Number of salt rounds for bcrypt
+      const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+      data.password = hashedPassword; // Replace the original password with the hashed one
+
+      const userdata = await collection.insertMany(data);
+      console.log(userdata);
+  }
+
+});
+// đăng nhập
+app.post("/login", async (req, res) => {
+  try {
+      const check = await collection.findOne({ name: req.body.username });
+      if (!check) {
+          res.send("User name cannot found")
+      }
+      // Compare the hashed password from the database with the plaintext password
+      const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+      if (!isPasswordMatch) {
+          res.send("wrong Password");
+      }
+      else {
+          res.render("home");
+      }
+  }
+  catch {
+      res.send("wrong Details");
+  }
+});
+
+app.listen(post, () => console.log(`Example app listening on post ${post}!`));
+
+
+
